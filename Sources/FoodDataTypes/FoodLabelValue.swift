055614147825fe7ca9cb2c05eba8b7439c7f61ee
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-29 13:11:10 +0500

    tag increment

--- Sources/FoodDataTypes/FoodLabelValue.swift
+++ Sources/FoodDataTypes/FoodLabelValue.swift
@@ -107,4 +107,133 @@ extension FoodLabelValue {
         public static let standardPattern =
         #"^(?:[^0-9.:]*(?: |\()|^\/?)([0-9.:]+)[ ]*(\#(units))+(?: .*|\).*$|\/?$)$"#
     }
+    
+    public static var DisqualifyingTexts: [String] = [
+        "contributes to a daily diet",
+        "daily value",
+        "produced for",
+        "keep frozen",
+        "best before",
+        "ingredients"
+//        "of which"
+    ]
+}
+
+//MARK: - Detect
+
+extension FoodLabelValue {
+    static func detect(in string: String, forScanner: Bool = true) -> [FoodLabelValue] {
+        detect(in: string, withPositions: false, forScanner: forScanner).map { $0.0 }
+    }
+    
+    /// Detects `Value`s in a provided `string` in the order that they appear
+    static func detect(in string: String, withPositions: Bool, forScanner: Bool = true) -> [(FoodLabelValue, Int)] {
+        
+        var string = string
+        /// Add regex to check if we have "Not detected" or "nil" and replace with 0 (no unit)
+//        if string.matchesRegex("(not detected|nil)") {
+//            return [(Value(amount: 0), 0), (Value(amount: 0), 1)]
+//        }
+//
+//        /// Or if we have "trace" replace it with 0.05 (no unit)
+//        if string.matchesRegex("(trace)") {
+//            return [(Value(amount: 0.05), 0)]
+//        }
+        
+        /// We invalidate these only for the `FoodLabelScanner` as they interfere with our tabular approach—so set this flag to `false` if you need to strictly grab all the values
+        if forScanner {
+            /// Invalidates strings like `20 g = 3`
+            if string.matchesRegex(#"[0-9]+[ ]*[A-z]*[ ]*=[ ]*[0-9]+"#) {
+                return []
+            }
+            
+            /// Invalidates strings like `5 x 3`
+            if string.matchesRegex(#"[0-9]+[ ]*x[ ]*[0-9]+"#) {
+                return []
+            }
+        }
+
+        for disqualifyingText in FoodLabelValue.DisqualifyingTexts {
+            guard !(string.lowercased().contains(disqualifyingText)) else {
+                return []
+            }
+        }
+        
+        /// Commented out in favor of the function based approach below (with additional ones with spaces)
+//        if string.hasSuffix("Og") {
+//            string = string.replacingLastOccurrence(of: "Og", with: "0g")
+//        }
+//        if string.hasSuffix("Omg") {
+//            string = string.replacingLastOccurrence(of: "Omg", with: "0mg")
+//        }
+//        if string.hasSuffix("Omcg") {
+//            string = string.replacingLastOccurrence(of: "Omcg", with: "0mcg")
+//        }
+        //TODO: Use regex for these, also include ones with simple "o"
+        func suffixReplace(_ incorrects: [String], with correct: String) {
+            for incorrect in incorrects {
+                if string.hasSuffix(incorrect) {
+                    string = string.replacingLastOccurrence(of: incorrect, with: correct)
+                    break
+                }
+            }
+        }
+        suffixReplace(["Og", "O g"], with: "0g")
+        suffixReplace(["Omg", "O mg"], with: "0mg")
+        suffixReplace(["Omcg", "O mcg"], with: "0mcg")
+
+        
+        func replace(_ incorrects: [String], with correct: String) {
+            for incorrect in incorrects {
+                if string == incorrect {
+                    string = correct
+                    break
+                }
+            }
+        }
+//        if string == "O" {
+//            string = "0"
+//        }
+        replace(["O"], with: "0")
+        replace(["O a"], with: "0g")
+
+        /// Replaces fractional values with their true values
+        string = string.replacingOccurrences(of: "1/2", with: "0.5")
+        string = string.replacingOccurrences(of: "1/3", with: "0.3333")
+        string = string.replacingOccurrences(of: "1/4", with: "0.25")
+        string = string.replacingOccurrences(of: "1/5", with: "0.2")
+
+        /// If we encouner strings such as `(%5 (A pall clues )) a` — which is usually how daily values in arabic is read—don't extract values from it
+//        guard !string.matchesRegex("%[0-9]+") else {
+//            return []
+//        }
+        
+        var array: [(value: FoodLabelValue, positionOfMatch: Int)] = []
+//        cprint("🔢      👁 detecting values in: \(string)")
+
+        let specialValuesArray = [
+            "(?<!not detected )(?:not detected)",
+            "(?:not detected)(?! not detected )",
+            "nil",
+            "trace"
+        ]
+        let specialValues = #"(\#(specialValuesArray.joined(separator: "|")))"#
+        let regex = #"(?:([0-9]+[0-9.,]*[ ]*(?:\#(FoodLabelValue.Regex.units)|)(?:[^A-z0-9]|]|$))|\#(specialValues))"#
+//        let regex = #"(?:([^A-z]|^)([0-9]+[0-9.,]*[ ]*(?:\#(Value.Regex.units)|)(?:[^A-z0-9]|]|$))|\#(specialValues))"#
+        if let matches = string.matches(for: regex), !matches.isEmpty {
+            
+            for match in matches {
+                guard let value = FoodLabelValue(fromString: match.string) else {
+//                    cprint("🔢      👁   - '\(match.string)' @ \(match.position): ⚠️ Couldn't get value")
+                    continue
+                }
+//                cprint("🔢      👁   - '\(match.string)' @ \(match.position): \(value.description)")
+                array.append((value, match.position))
+            }
+        }
+        
+        array.sort(by: { $0.positionOfMatch < $1.positionOfMatch })
+        return array
+    }
+
 }

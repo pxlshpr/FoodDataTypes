2936c74a6376335b0da5640ecf1836c9113ace9f
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-03 12:25:58 +0500

    tag increment

--- Sources/FoodDataTypes/SearchTokens/SearchRank.swift
+++ Sources/FoodDataTypes/SearchTokens/SearchRank.swift
@@ -1,23 +1,34 @@
 import Foundation
 
-public enum SearchRank: Int, Codable, CaseIterable {
+public enum SearchRank: Int, Codable {
     case none       = -1
-    case highest    = 4
-    case higher     = 3
-    case high       = 2
-    case standard   = 1
     case low        = 0
+    case standard   = 1
+    case high       = 2
+    case higher     = 3
+    case highest    = 4
 }
 
 public extension SearchRank {
+    static var allPriorities: [SearchRank] {
+        [.low, .standard, .high, .higher, .highest]
+    }
+
     var description: String {
         switch self {
-        case .none:     "Not Included"
-        case .low:      "Low Priority"
-        case .standard: "Medium Priority"
-        case .high:     "High Priority"
-        case .higher:   "Higher Priority"
-        case .highest:  "Highest Priority"
+        case .none:     "Remove"
+        case .low:      "Low"
+        case .standard: "Standard"
+        case .high:     "High"
+        case .higher:   "Higher"
+        case .highest:  "Highest"
+        }
+    }
+    
+    var systemImage: String {
+        switch self {
+        case .none: "minus.circle"
+        default:    "\(self.rawValue).square"
         }
     }
 }

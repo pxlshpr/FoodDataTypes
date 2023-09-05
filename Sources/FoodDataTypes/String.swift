f4880d7f54860aa2a17516226f6ee55c9c2426fd
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-05 22:20:15 +0500

    tag increment

--- Sources/FoodDataTypes/String.swift
+++ Sources/FoodDataTypes/String.swift
@@ -9,3 +9,36 @@ public extension String {
         FoodLabelValue.detect(in: self, forScanner: false)
     }
 }
+
+extension String {
+    
+    func sanitizedSearchWord() -> String {
+        let invalidCharacters = CharacterSet
+            .whitespacesAndNewlines
+            .union(.controlCharacters)
+            .union(.illegalCharacters)
+            .union(.decimalDigits)
+            .union(.punctuationCharacters)
+            .union(.symbols)
+        return self
+            .lowercased()
+            .components(separatedBy: invalidCharacters)
+            .joined()
+    }
+}
+
+extension String {
+    var searchWords: [String] {
+//        self
+//            .replacingOccurrences(of: ",", with: " ")
+//            .replacingOccurrences(of: ".", with: " ")
+//            .components(separatedBy: " ")
+//            .map { $0.replacingOccurrences(of: " ", with: "") } /// handles double, triple, etc whitespaces
+//            .compactMap { $0.isEmpty ? nil : $0 }
+        
+        self
+            .components(separatedBy: " ")
+            .map { $0.sanitizedSearchWord() }
+            .compactMap { $0.isEmpty ? nil : $0 }
+    }
+}
